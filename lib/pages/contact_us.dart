11321aa079cfeb7d3e694fbb169aae9efbe3cb61
zejun1219 <mazejun@graduate.utm.my>
2024-04-26 22:40:56 +0800

    contact us.

--- lib/pages/contact_us.dart
+++ lib/pages/contact_us.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class ContactUsPage extends StatefulWidget {
-  const ContactUsPage({Key? key}) : super(key: key);
+class ContactUsPage extends StatelessWidget {
+  ContactUsPage({Key? key}) : super(key: key);
 
-  @override
-  State<ContactUsPage> createState() => _ContactUsPageState();
-}
-
-class _ContactUsPageState extends State<ContactUsPage> {
   // Method to launch URLs
   void _launchURL(String url) async {
     if (!await launch(url)) {
@@ -18,35 +13,26 @@ class _ContactUsPageState extends State<ContactUsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Contact Us Page'),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-              icon: Image.asset(
-                'lib/images/pizza.png',
-                height: 80,
-              ), // Place your WhatsApp icon in the assets folder
-              onPressed: () => _launchURL(
-                  'https://wa.me/yourphonenumber'), // Replace with your phone number
-            ),
-            SizedBox(width: 20), // Space between icons
-            IconButton(
-              icon: Image.asset(
-                'lib/images/pizza.png',
-                height: 80,
-              ), // Place your Facebook icon in the assets folder
-              onPressed: () => _launchURL(
-                  'https://www.facebook.com/yourprofile'), // Replace with your profile link
-            ),
-          ],
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.chat,
+            size: 40.0,
+          ),
+          onPressed: () => _launchURL('https://wa.me/+601161220980'),
         ),
-      ),
+        SizedBox(width: 20), // 图标间距
+        IconButton(
+          icon: Icon(
+            Icons.face,
+            size: 40.0,
+          ),
+          onPressed: () => _launchURL(
+              'https://www.facebook.com/profile.php?id=100069972000599'),
+        ),
+      ],
     );
   }
 }
-//hah
