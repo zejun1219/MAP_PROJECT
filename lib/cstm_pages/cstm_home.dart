00953c7475d6f9a7c207617ea43032dbdc142992
zejun1219 <118799534+zejun1219@users.noreply.github.com>
2024-05-25 23:05:34 +0800

    Update cstm_home.dart
    update cstm_home.dart

--- lib/cstm_pages/cstm_home.dart
+++ lib/cstm_pages/cstm_home.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:template01/cstm_pages/bread.dart';
-import 'package:template01/cstm_pages/combo.dart';
-import 'package:template01/cstm_pages/dessert.dart';
-import 'package:template01/cstm_pages/drinks.dart';
-import 'package:template01/cstm_pages/pizza.dart';
-// import 'package:template01/cstm_pages/shopping_cart.dart';
-import 'package:template01/cstm_pages/snack.dart';
-import 'package:template01/cstm_pages/soup.dart';
-import 'package:template01/cstm_pages/stable_food.dart';
-import 'package:template01/cstm_pages/steak.dart';
-// import 'package:template01/models/dishes.dart';
 import 'package:template01/models/user.dart';
-import 'package:template01/pages/login.dart';
-import 'package:template01/pages/my_profile.dart';
-// customer manage products page
+import 'package:template01/components/cstm_home_com.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class CustomerHome extends StatefulWidget {
-  final User user; // 添加这一行
+  final User user;
 
   const CustomerHome({
     Key? key,
-    required this.user, // 添加这一行
+    required this.user,
   }) : super(key: key);
 
   @override
@@ -27,6 +16,66 @@ class CustomerHome extends StatefulWidget {
 }
 
 class _CustomerHomeState extends State<CustomerHome> {
+  final List<String> images = [
+    'lib/images/Pine meat.jpeg',
+    'lib/images/durain.jpeg',
+    'lib/images/seafood.jpeg',
+    // 添加更多图片的 URL
+  ];
+
+  int _currentPage = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      initialPage: _currentPage,
+      viewportFraction: 0.6, // 设置视口占比为0.6
+    );
+    _startAutoPageChange();
+  }
+
+  void _startAutoPageChange() {
+    Future.delayed(Duration(seconds: 3), () {
+      if (mounted) {
+        setState(() {
+          _currentPage = (_currentPage + 1) % images.length;
+          _pageController.animateToPage(
+            _currentPage,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+          );
+          _startAutoPageChange();
+        });
+      }
+    });
+  }
+
+  void _launchTwitter() async {
+    const url = 'https://twitter.com/utm_my';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _launchFacebook() async {
+    const url = 'https://www.facebook.com/univteknologimalaysia';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,112 +83,92 @@ class _CustomerHomeState extends State<CustomerHome> {
         title: Text('Customer Home Page'),
         backgroundColor: Colors.deepPurple,
       ),
-      drawer: Drawer(
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-//? ------------ drawer go to components --------------------------
-            DrawerHeader(
-              child: Icon(Icons.settings_accessibility, size: 60),
+      drawer: CustomDrawer(user: widget.user),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Welcome to the Pizza House!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Profile'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyProfilePage(
-                            user: widget.user))); // 使用 widget.user
-              },
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              child: PageView.builder(
+                itemCount: images.length,
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Transform.scale(
+                    scale: index == _currentPage ? 1 : 0.8, // 当前页全尺寸，其他页缩小
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10), // 添加水平间距
+                      child: Image.asset(
+                        images[index],
+                        fit: BoxFit.cover, // 更改为 cover 以填满容器
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.local_pizza),
-              title: Text('Pizza'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            PizzaPage(user: widget.user))); // 使用 widget.user
-              },
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'We are delighted to serve you with the best pizzas in town. Our pizzas are made with love and fresh ingredients, ensuring a delightful experience with every bite.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.fastfood),
-              title: Text('Combo'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ComboPage()));
-              },
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                'Explore our menu and enjoy your favorite pizza!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple,
+                ),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.local_dining),
-              title: Text('Snack'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SnackPage()));
-              },
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.alternate_email),
+                  color: Colors.blue,
+                  iconSize: 40,
+                  onPressed: _launchTwitter,
+                ),
+                IconButton(
+                  icon: Icon(Icons.facebook),
+                  color: Colors.blue,
+                  iconSize: 40,
+
+                  onPressed: _launchFacebook,
+                ),
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.local_bar),
-              title: Text('Steak'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SteakPage()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.local_pizza),
-              title: Text('Bread'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => BreadPage()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.icecream),
-              title: Text('Dessert'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => DessertPage()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.local_drink),
-              title: Text('Drinks'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => DrinksPage()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.soup_kitchen),
-              title: Text('Soup'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SoupPage()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.restaurant),
-              title: Text('Stable Food'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => StableFoodPage()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text("Logout"),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-            ),
+            SizedBox(height: 20),
           ],
         ),
       ),
     );
   }
 }
+
