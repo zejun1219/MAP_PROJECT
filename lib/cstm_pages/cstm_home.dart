37ced2499477301df041c85f67d6798bc60c0eb8
XiaolongYin55 <xiaolong@graduate.utm.my>
2024-06-08 18:42:09 +0800

    modify cstm home

--- lib/cstm_pages/cstm_home.dart
+++ lib/cstm_pages/cstm_home.dart
@@ -7,7 +7,7 @@ import 'package:template01/models/user.dart';
 import 'package:template01/components/cstm_drawer.dart';
 import 'package:template01/services/firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
-// modify customer page
+// modify customer home page
 class CustomerHome extends StatefulWidget {
   final Users user;
   const CustomerHome({
